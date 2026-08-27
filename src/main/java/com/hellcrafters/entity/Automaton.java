16d6f6f833e15b47eb74d8f1bb744b160c697e0c
Zach <Zachary.Hartfelder@outlook.com>
2026-08-26 20:46:50 -0500

    Finished learning nbt reading, arrows will not interact with custom hitboxes when in ground. Looking into making my first mixin so projectiles detect hitting custom hitboxes too

--- src/main/java/com/hellcrafters/entity/Automaton.java
+++ src/main/java/com/hellcrafters/entity/Automaton.java
@@ -1,14 +1,24 @@
 package com.hellcrafters.entity;
 
 import com.hellcrafters.damage.ArmorValue;
+import dev.xylonity.knightlib.api.entity.hitbox.BoneHitbox;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
 import software.bernie.geckolib.animatable.GeoEntity;
 
+import java.util.function.BiConsumer;
+
 public abstract class Automaton extends PathfinderMob implements GeoEntity {
     public static final ArmorValue defaultArmorValue = ArmorValue.LIGHT;
+    private final BiConsumer<BoneHitbox, Entity> onHit;
 
-    protected Automaton(EntityType<? extends PathfinderMob> entityType, Level level) {super(entityType, level);}
+    protected Automaton(EntityType<? extends PathfinderMob> entityType, Level level, BiConsumer<BoneHitbox, Entity> onHit) {
+        super(entityType, level);
+        this.onHit = onHit;
+    }
+
+
 
 }
--- src/main/java/com/hellcrafters/util/Collisions.java
+++ src/main/java/com/hellcrafters/util/Collisions.java
@@ -1,29 +1,26 @@
 package com.hellcrafters.util;
 
-import com.hellcrafters.HellCrafters;
-import dev.xylonity.knightlib.api.entity.hitbox.BoneHitbox;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.phys.EntityHitResult;
 
-import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class Collisions {
     public static Predicate<Entity> projectileCollisionFilter = entity -> {
-        if (entity instanceof Projectile) {
-            if (entity instanceof AbstractArrow) {
-                //HellCrafters.LOGGER.info(entity.getEntityData().toString());
-                return !entity.getPersistentData().getBoolean("inGround");
-            }
-            return true;
+        // cancels early for various specific entity type checks
+        // we only want things that should interact with hitboxes in a way
+        // that physically affects them
+        if(!(entity instanceof Projectile)) return false;
+
+        Boolean returnValue = false;
+
+        // prevents detection if the arrow's hitbox is currently in the ground
+        if (entity instanceof AbstractArrow arrow) {
+            returnValue = !arrow.saveWithoutId(new CompoundTag()).getBoolean("inGround");
         }
-        return false;
-    };
 
-
-    public static BiConsumer<BoneHitbox, Entity> hit = (hitBox, target) -> {
+        return returnValue;
     };
 }
