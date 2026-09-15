0562d52ce2e98ef3767aae0669d1d8849d96ff3b
disco <2t5rgyzpg2@privaterelay.appleid.com>
2026-09-15 13:21:54 -0500

    Cleaned up all redundant dependencies

--- src/main/java/com/hellcrafters/entity/HellcrafterEntity.java
+++ src/main/java/com/hellcrafters/entity/HellcrafterEntity.java
@@ -1,8 +1,6 @@
 package com.hellcrafters.entity;
 
 import com.hellcrafters.HellCrafters;
-import dev.customhitboxlib.api.ICustomMultipart;
-import dev.customhitboxlib.api.PartDefinition;
 import dev.xylonity.knightlib.api.entity.hitbox.BoneHitboxHolder;
 import dev.xylonity.knightlib.api.entity.hitbox.BoneHitboxManager;
 import net.minecraft.resources.ResourceLocation;
@@ -12,7 +10,6 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.entity.PartEntity;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -26,7 +23,7 @@ import java.util.List;
 
 // This entity is our base for every entity in this mod. Every entity will utilize geckolib for animations,
 // and at least have the ability for extended hitboxes.
-public class HellcrafterEntity extends PathfinderMob implements GeoEntity, BoneHitboxHolder, ICustomMultipart {
+public class HellcrafterEntity extends PathfinderMob implements GeoEntity, BoneHitboxHolder {
 
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
     private final BoneHitboxManager hitboxManager = new BoneHitboxManager(this);
@@ -66,18 +63,7 @@ public class HellcrafterEntity extends PathfinderMob implements GeoEntity, BoneH
 
 
 
-        /*
-        // loops through every bone name with an added hitbox, updating the position of the OBB hitbox
-        additionalHitboxes.forEach(boneName -> {
-            HellCrafters.LOGGER.info(this.stringUUID);
-            HellCrafters.LOGGER.info(boneName);
-            //HellCrafters.LOGGER.info(geoModel.getBone(boneName).get().toString());
-            //HellCrafters.LOGGER.info(geoCache.getManagerForId(this.getId()).);
-            //HellCrafters.LOGGER.info(boneSnapshot);//.getBone().getScaleVector().toString());
-            //HellCrafters.LOGGER.info(boneSnapshot.getBone().getWorldSpaceMatrix().toString());
-            //OBB.updateHitbox(geoCache.getManagerForId(this.getId()).getBoneSnapshotCollection().get(boneName));
-        });
-         */
+
     }
 
     @Override
@@ -96,101 +82,8 @@ public class HellcrafterEntity extends PathfinderMob implements GeoEntity, BoneH
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {}
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() { return this.geoCache; }
-
     @Override
     public @Nullable BoneHitboxManager getBoneHitboxManager() {
         return hitboxManager;
     }
-
-    /**
-     * @return Returns a list of PartEntities
-     */
-    @Override
-    public @Nullable PartEntity<?>[] getCustomParts() {
-        return new PartEntity[0];
-    }
-
-    /**
-     *  This method by defualt repositions all parts using their positioners
-     */
-    @Override
-    public void tickCustomParts() {
-    }
-
-    /**
-     * Takes in a String name and a PartDefinition record to add a custom part to the model
-     * @param name
-     * @param partDefinition
-     */
-    @Override
-    public void addCustomPart(String name, PartDefinition partDefinition) {
-
-    }
-
-    /**
-     * @param s
-     */
-    @Override
-    public void removeCustomPart(String s) {
-
-    }
-
-    /**
-     * Determines whether
-     * @return
-     */
-    @Override
-    public boolean hasCustomParts() {
-        return true;
-    }
-
-    /**
-     * Sets whether the main hitbox can be targeted
-     * @param b
-     */
-    @Override
-    public void setMainHitboxPickable(boolean b) {}
-
-    /**
-     * @return Whether the main hitbox is targetable
-     */
-    @Override
-    public boolean isMainHitboxPickable() {
-        return false;
-    }
-
-    /**
-     * Sets whether the main hitbox pushes entities
-     * @param b
-     */
-    @Override
-    public void setMainHitboxPushable(boolean b) {}
-
-    /**
-     * Whether the main hitbox pushes entities
-     * @return
-     */
-    @Override
-    public boolean isMainHitboxPushable() {
-        return true;
-    }
-
-    /**
-     * Sets whether the main hitbox collides with blocks
-     * @param b
-     */
-    @Override
-    public void setMainHitboxCollision(boolean b) {
-
-    }
-
-    /**
-     * Whether the main hitbox collides with blocks
-     * @return
-     */
-    @Override
-    public boolean isMainHitboxCollision() {
-        return true;
-    }
-
 }
